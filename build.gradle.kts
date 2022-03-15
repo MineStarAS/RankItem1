a072c618d0b61acafd1b52b19b740004ef62ce66
MineStar <band1019@naver.com>
2022-03-15 20:56:15 +0900

    Changes by projecttl

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,43 @@
 plugins {
     kotlin("jvm") version "1.6.10"
     id("com.github.johnrengelman.shadow") version "7.1.0"
-    id("org.jetbrains.dokka") version "1.5.0"
-    `maven-publish`
 }
 
 group = "kr.kro.minestar"
 version = "1.0.0"
-val plugins = File("C:\\Users\\MineStar\\Desktop\\MC Server folder\\MCserver 1.18.1 - MineFarmWorld\\plugins")
 
-repositories {
-    mavenCentral()
-    maven("https://papermc.io/repo/repository/maven-public/")
-    maven(url = "https://oss.sonatype.org/content/repositories/snapshots/") {
-        name = "sonatype-oss-snapshots"
+allprojects {
+    apply(plugin = "org.jetbrains.kotlin.jvm")
+    apply(plugin = "com.github.johnrengelman.shadow")
+
+    repositories {
+        mavenCentral()
     }
-    maven("https://repo.projecttl.net/repository/maven-public/")
+
+    java {
+        toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+
+subprojects {
+    repositories {
+        mavenCentral()
+        maven("https://papermc.io/repo/repository/maven-public/")
+        maven(url = "https://oss.sonatype.org/content/repositories/snapshots/") {
+            name = "sonatype-oss-snapshots"
+        }
+        maven("https://repo.projecttl.net/repository/maven-public/")
+    }
+
+    dependencies {
+        implementation(kotlin("stdlib"))
+        compileOnly("net.kyori:adventure-api:4.10.1")
+        compileOnly("io.papermc.paper:paper-api:1.18.1-R0.1-SNAPSHOT")
+        compileOnly("com.arcaniax:HeadDatabase-API:1.3.1")
+
+        //project_TL
+
+        //MineStar
+        compileOnly("kr.kro.minestar:Utility-API:1.6.3")
+    }
 }
